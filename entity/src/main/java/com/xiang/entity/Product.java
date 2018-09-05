b99274559fe0b3f5ff534e9716ef6128dbdcd414
Administrator <gxiang0208@126.com>
2018-09-05 22:33:59 +0800

    基础框架搭建

---
+++ entity/src/main/java/com/xiang/entity/Product.java
@@ -0,0 +1,158 @@
+package com.xiang.entity;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * 产品
+ */
+@Entity
+public class Product
+{
+    //编号
+    @Id
+    private String id;
+    //名称
+    private String name;
+    /**
+     * @see com.xiang.entity.enums.ProductStatus
+     */
+    private String status;
+    //起投金额
+    private BigDecimal thresholdAmount;
+    //投资步长
+    private BigDecimal stepAmount;
+    //锁定期，Interger而不是int类型为了区分原始值
+    private Integer lockTerm;
+    //收益率
+    private BigDecimal rewardRate;
+    //备注
+    private String memo;
+    //创建时间
+    private Date createAt;
+    //创建者ID
+    private String createUser;
+    //更新时间
+    private Date updateAt;
+    //更新者ID
+    private String updateUser;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public BigDecimal getThresholdAmount() {
+        return thresholdAmount;
+    }
+
+    public void setThresholdAmount(BigDecimal thresholdAmount) {
+        this.thresholdAmount = thresholdAmount;
+    }
+
+    public BigDecimal getStepAmount() {
+        return stepAmount;
+    }
+
+    public void setStepAmount(BigDecimal stepAmount) {
+        this.stepAmount = stepAmount;
+    }
+
+    public Integer getLockTerm() {
+        return lockTerm;
+    }
+
+    public void setLockTerm(Integer lockTerm) {
+        this.lockTerm = lockTerm;
+    }
+
+    public BigDecimal getRewardRate() {
+        return rewardRate;
+    }
+
+    public void setRewardRate(BigDecimal rewardRate) {
+        this.rewardRate = rewardRate;
+    }
+
+    public String getMemo() {
+        return memo;
+    }
+
+    public void setMemo(String memo) {
+        this.memo = memo;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    public String getUpdateUser() {
+        return updateUser;
+    }
+
+    public void setUpdateUser(String updateUser) {
+        this.updateUser = updateUser;
+    }
+
+    @Override
+    public String toString()
+    {
+        //return ReflectionToStringBuilder.toString(this); 性能比下面的差，但微乎其微
+        return "Product{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", status='" + status + '\'' +
+                ", thresholdAmount=" + thresholdAmount +
+                ", stepAmount=" + stepAmount +
+                ", lockTerm=" + lockTerm +
+                ", rewardRate=" + rewardRate +
+                ", memo='" + memo + '\'' +
+                ", createAt=" + createAt +
+                ", createUser='" + createUser + '\'' +
+                ", updateAt=" + updateAt +
+                ", updateUser='" + updateUser + '\'' +
+                '}';
+    }
+}
---
+++ manager/src/main/java/com/xiang/manager/controller/ProductController.java
@@ -0,0 +1,69 @@
+package com.xiang.manager.controller;
+
+import com.xiang.entity.Product;
+import com.xiang.manager.service.ProductService;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController
+{
+
+    private static Logger LOG = LoggerFactory.getLogger(ProductController.class);
+
+    @Autowired
+    private ProductService service;
+
+    @RequestMapping(value = "" ,method = RequestMethod.POST)
+    public Product addProduct(@RequestBody Product product)
+    {
+        LOG.info("创建产品，参数：{}",product);
+        Product result = service.addProduct(product);
+        LOG.info("创建产品，结果：{}",product);
+        return result;
+    }
+
+    @RequestMapping(value = "/{id}",method = RequestMethod.GET)
+    public Product findOne(@PathVariable String id)
+    {
+        LOG.info("查询单个产品，id={}",id);
+        Product product = service.findOne(id);
+        LOG.info("查询单个产品，结果={}",product);
+        return product;
+    }
+
+    /**
+     *
+     * @return
+     */
+    @RequestMapping(value = "",method = RequestMethod.GET)
+    public Page<Product> query(String ids, BigDecimal minRewardRate, BigDecimal maxRewarRate, String status, int pageNum, int pageSize)
+    {
+        LOG.info("查询产品，ids={},minRewardRate={},minRewardRate={},status={},pageNum={},pageSize={}",ids,minRewardRate,maxRewarRate,status,pageNum,pageSize);
+        List<String> idList = null , statusList = null;
+        if(!StringUtils.isEmpty(ids))
+        {
+            idList = Arrays.asList(ids.split(","));
+        }
+        if(!StringUtils.isEmpty(status))
+        {
+            statusList = Arrays.asList(status.split(","));
+        }
+        Pageable pageable = new PageRequest(pageNum,pageSize);
+        Page<Product> results = service.query(idList,minRewardRate,maxRewarRate,statusList,pageable);
+        LOG.info("查询产品，结果={}",results);
+        return results;
+    }
+
+}
